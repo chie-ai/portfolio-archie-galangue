7e9ecdc5666a80c41fd3e3c1fab539dce6ba9519
chie-ai <bacuetesarchie@gmail.com>
2024-02-08 08:16:36 +0800

    Map asset images

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useImagesStore } from '@/stores/images'
+
+const imageStore = useImagesStore()
+
+onBeforeMount(() => {
+  console.log('Project images: ', import.meta.glob('@/assets/projects/*.png', { eager: true }))
+  imageStore.setImages(import.meta.glob('@/assets/projects/*.png', { eager: true }))
+})
+</script>
